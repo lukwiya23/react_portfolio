b79bade96b7f8f62008f3be2d68376ad7d0e013a
lukwiya23 <luk23bonnie8@gmail.com>
2021-05-09 20:25:11 +0300

    Faq section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+function FaqSection() {
+    return (
+       <div className="Faq">
+           <h2>Any questions <span>FAQ</span></h2>
+           <div className="question">
+               <h4>How do I start?</h4>
+               <div className="answer">
+                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia dignissimos voluptate, error repudiandae libero voluptas?</p>
+               </div>
+           </div>
+           <div className="question">
+               <h4>Daily Schedule</h4>
+               <div className="answer">
+                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia dignissimos voluptate, error repudiandae libero voluptas?</p>
+               </div>
+           </div>
+           <div className="question">
+               <h4>Payment Methods</h4>
+               <div className="answer">
+                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia dignissimos voluptate, error repudiandae libero voluptas?</p>
+               </div>
+           </div>
+           <div className="question">
+               <h4>Products Offered</h4>
+               <div className="answer">
+                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia dignissimos voluptate, error repudiandae libero voluptas?</p>
+               </div>
+           </div>
+       </div>
+    )
+}
+
+export default FaqSection
--- src/components/ServicesSection.js
+++ src/components/ServicesSection.js
@@ -2,8 +2,37 @@ import React from 'react'
 
 function ServicesSection() {
     return (
-        <div>
-            
+        <div className="services">
+            <div className="description">
+                <h2>High <span>Quality</span>services.</h2>
+                <div className="cards">
+                    <div className="card">
+                        <div className="icon"><img src="/images/clock.svg" alt="" />
+                        <h3>Efficiency</h3>
+                        </div>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum repudiandae explicabo commodi numquam sed, officiis doloribus dignissimos mollitia deleniti facere?</p>
+                    </div>
+                    <div className="card">
+                        <div className="icon"><img src="/images/teamwork.svg" alt="" />
+                        <h3>Teamwork</h3>
+                        </div>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum repudiandae explicabo commodi numquam sed, officiis doloribus dignissimos mollitia deleniti facere?</p>
+                    </div>
+                    <div className="card">
+                        <div className="icon"><img src="/images/diaphragm.svg" alt="" />
+                        <h3>Diaphragm</h3>
+                        </div>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum repudiandae explicabo commodi numquam sed, officiis doloribus dignissimos mollitia deleniti facere?</p>
+                    </div>
+                    <div className="card">
+                        <div className="icon"><img src="/images/money.svg" alt="" />
+                        <h3>Affordable</h3>
+                        </div>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum repudiandae explicabo commodi numquam sed, officiis doloribus dignissimos mollitia deleniti facere?</p>
+                    </div>
+                </div>
+                <img src="/images/home2.png" alt="" />
+            </div>
         </div>
     )
 }
