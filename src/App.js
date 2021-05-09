c9e27c9ff1abdabc499152c8d10250970375bda6
lukwiya23 <luk23bonnie8@gmail.com>
2021-05-09 16:15:14 +0300

    imported about section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
+import AboutSection from './components/AboutSection'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Film</h1>
+      < AboutSection  />
     </div>
   );
 }
