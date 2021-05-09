a15e9ae73cb8b83fdc86a0e42511eb0b309cfeb4
lukwiya23 <luk23bonnie8@gmail.com>
2021-05-09 16:11:18 +0300

    setting up about section

---
+++ src/components/AboutSection.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function AboutSection() {
+    return (
+        <div>
+            <div className="description">
+                <div className="title">
+                    <div className="hide">
+                        <h2>We work to make</h2>
+                    </div>
+                    <div className="hide">
+                        <h2>We work to make</h2>
+                    </div>
+                    <div className="hide">
+                        <h2>Your <span>dreams</span>come</h2>
+                    </div>
+                    <div className="hide">
+                        <h2>true. </h2>
+                    </div>
+                </div>
+                <p>Contact us for any photography or videography ideas you would like us to bring to life. We have a team of professionals at your disposal.</p>
+                <button>Contact Us</button>
+            </div>
+            
+        </div>
+    )
+}
+
+export default AboutSection
