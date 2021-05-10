78603ff407e8338c981fe6107bea1501f15ad359
lukwiya23 <luk23bonnie8@gmail.com>
2021-05-10 19:49:31 +0300

    addding styling to about section

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -4,25 +4,25 @@ import styled from 'styled-components'
 function AboutSection() {
     return (
         <About>
-            <div className="description">
+            <Description>
                 <div className="title">
                     <div className="hide">
                         <h2>We work to make</h2>
                     </div>
                   
-                    <div className="hide">
+                    <Hide>
                         <h2>Your <span>dreams </span>come</h2>
-                    </div>
+                    </Hide>
                     <div className="hide">
                         <h2>true. </h2>
                     </div>
                 </div>
                 <p>Contact us for any photography or videography ideas you would like us to bring to life. We have a team of professionals at your disposal.</p>
                 <button>Contact Us</button>
-            </div>
-            <div className="image">
+            </Description>
+            <Image>
                 <img src="/images/home1.png" alt="" />
-            </div>
+            </Image>
             
         </About>
     )
@@ -39,5 +39,24 @@ color:white;
 
 `
 
+const Description = styled.div`
+flex:1;
+padding-right:5rem;
+`
+const Image = styled.div`
+flex:1;
+overflow:hidden;
+
+img{
+    width:100%;
+    height:80vh;
+    object-fit:cover;
+}
+`
+
+const Hide = styled.div`
+
+overflow:hidden;
+`
 export default AboutSection
 
