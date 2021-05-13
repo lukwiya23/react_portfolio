f3228750073c1fe90c31de46f7fa2cbcc0e3f520
lukwiya23 <luk23bonnie8@gmail.com>
2021-05-13 20:03:04 +0300

    .

--- src/App.js
+++ src/App.js
@@ -2,13 +2,28 @@ import AboutUs from './pages/AboutUs'
 import GlobalStyle from './components/GlobalStyles'
 import './App.css';
 import NavBar from './components/NavBar'
+import ContactUs from './pages/ContactUs'
+import OurWork from './pages/OurWork'
+import {Switch, Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
       <GlobalStyle />
+      <NavBar />
+      <Switch>    
+      <Route path="/" exact>
       < AboutUs  />
+      </Route>
+      <Route path="/work" >
+ 
+      < OurWork  />
+      </Route>
+      <Route path="/contact">
+      < ContactUs  />
+      </Route>
+      </Switch>
+
     </div>
   );
 }
---
+++ src/pages/OurWork.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function OurWork() {
+    return (
+        <div>
+            Our Work
+            
+        </div>
+    )
+}
+
+export default OurWork
