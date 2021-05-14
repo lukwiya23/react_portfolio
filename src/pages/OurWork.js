4de85b41adc8c107807960c1ee1e8c3d6a3e238d
lukwiya23 <luk23bonnie8@gmail.com>
2021-05-14 21:47:48 +0300

    building our work page

--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -1,10 +1,34 @@
 import React from 'react'
 import styled from 'styled-components'
+import {Link} from 'react-router-dom'
 
 function OurWork() {
     return (
         <Work>
-            <h1>Our work</h1>
+            <Movie>
+                <h2>The Athlete</h2>
+                <div className="line"></div>
+                    <Link>
+                    <img src="/images/athlete-small.png" alt="" />
+                    </Link>
+               
+            </Movie>
+            <Movie>
+                <h2>The Racer</h2>
+                <div className="line"></div>
+                    <Link>
+                    <img src="/images/theracer-small.png" alt="" />
+                    </Link>
+                
+            </Movie>
+            <Movie>
+                <h2>Good Times</h2>
+                <div className="line"></div>
+                    <Link>
+                    <img src="/images/goodtimes-small.png" alt="" />
+                    </Link>
+                
+            </Movie>
         </Work>
     )
 }
@@ -12,6 +36,26 @@ function OurWork() {
 
 const Work = styled.div`
 min-height: 100vh;
+overflow:hidden;
+padding: 5rem 10rem;
+h2{
+    padding: 1rem 0rem;
+}
 
 `
+const Movie = styled.div`
+padding-bottom:10rem;
+.line{
+    height:0.5rem;
+    background:#cccccc;
+    margin-bottom:3rem;
+
+    img{
+        width:100%;
+        height:70vh;
+        object-fit:cover;
+    }
+}
+`
+
 export default OurWork
